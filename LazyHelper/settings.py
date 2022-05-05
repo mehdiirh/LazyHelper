876607ce7accceb61198581c5604de96964ee6a3
Mehdi Rahimi <mehdii.rh78@gmail.com>
2022-05-05 21:05:10 +0430

    Some minor changes in SECRET_KEY

--- LazyHelper/settings.py
+++ LazyHelper/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'DJANGO_INSECURE(`-._!$(#)$!===*YOUR_SECRET_KEY_GOES_HERE*===!$(#)$!*_.-`)'
+SECRET_KEY = 'DJANGO_INSECURE(`-._!$(#)$!===*YOUR_SECRET_KEY*===!$(#)$!*_.-`)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
