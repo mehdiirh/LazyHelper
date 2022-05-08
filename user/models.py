66cb7256ff8504374e0e58b5eee3b4020af6109d
Mehdi Rahimi <mehdii.rh78@gmail.com>
2022-05-08 16:40:17 +0430

    Add help text to Profile/api_key field

--- user/models.py
+++ user/models.py
@@ -7,7 +7,8 @@ from utils.generators.functions import generate_unique_id
 class Profile(models.Model):
 
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-    api_key = models.CharField(max_length=32, null=True)
+    api_key = models.CharField(max_length=32, null=True, help_text='To change api_key, remove value of'
+                                                                   ' this input and save.')
 
     def save(self, *args, **kwargs):
         if self.api_key is None or len(str(self.api_key)) < 32:
